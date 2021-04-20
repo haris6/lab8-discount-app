681e4d7b0db6fe33944e0a4d343d754926d2fe42
Haris Akram <harisakram593@gmail.com>
2021-04-20 21:03:41 +0500

    task-4

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput  } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
 
@@ -19,14 +19,30 @@ export default function App() {
       return final;
     }
   }
+  
+  const checkPrice = (a) => {
+    if (a >= 0){
+      setPrice(a);
+    }
+  }
+
+  const checkDisc = (a) => {
+    if(a >= 0 && a <= 100){
+      setDisc(a);
+    }
+  }
+  
 
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} onChangeText={setPrice} value={Price} placeholder="Enter Price" keyboardType="numeric"/>
-      <TextInput style={styles.input} onChangeText={setDisc} value={Disc} placeholder="Enter Discount %" keyboardType="numeric"/>
-      <Text>You Save : $ {Calculate("s")} </Text>
-      <Text>Final Price : $ {Calculate("f")}</Text>
-      <StatusBar style="auto" />
+      <View style ={styles.box} >
+        <Text style={{fontSize:30, margin:30, color:"#F1C40F", fontWeight:"bold"}}>Discount Calculator</Text> 
+        <TextInput style={styles.input} onChangeText={(n) => {checkPrice(n)}} value={Price} placeholder="Enter Price" keyboardType="numeric"/>
+        <TextInput style={styles.input} onChangeText={(n) => {checkDisc(n)}} value={Disc} placeholder="Enter Discount %" keyboardType="numeric"/>
+        <Text style = {styles.display}>You Save : $ {parseFloat(Calculate("s")).toFixed(2)} </Text>
+        <Text style = {styles.display}>Final Price : $ {parseFloat(Calculate("f")).toFixed(2)}</Text>
+        <StatusBar style="auto"/>
+      </View> 
     </View>
   );
 }
@@ -34,15 +50,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F7F9F9',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
+  box:{
+    borderColor:"firebrick",
+    borderWidth:3,
+    borderRadius:25,
+    backgroundColor:"#212F3D",
   },
 
+  input: {
+    color:"#1C2833",
+    fontWeight:"bold",
+    height: 30,
+    margin: 30,
+    fontSize:15,
+    borderWidth:3,
+    borderColor:"firebrick",
+    borderRadius:10, 
+    backgroundColor:"#fff",
+    marginBottom:10,
+  },
+
+  display:{
+    color:"#F1C40F",
+    fontSize:15,
+    marginLeft:80,
+    marginRught:50,
+    marginBottom:30,
+    marginTop:30,
+  }
 });
