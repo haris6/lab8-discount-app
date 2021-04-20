fc84e192e766f55bbceac579f31bacafef264c3e
Haris Akram <harisakram593@gmail.com>
2021-04-20 18:25:48 +0500

    task-3

--- App.js
+++ App.js
@@ -8,10 +8,24 @@ export default function App() {
 
   const [Disc,setDisc] = useState("");
 
+  const Calculate = (n) => {
+    var a = Disc/100;
+    var save = a * Price;
+    var final = Price - save;
+    if(n == "s"){
+      return save;
+    }
+    if(n == "f"){
+      return final;
+    }
+  }
+
   return (
     <View style={styles.container}>
       <TextInput style={styles.input} onChangeText={setPrice} value={Price} placeholder="Enter Price" keyboardType="numeric"/>
       <TextInput style={styles.input} onChangeText={setDisc} value={Disc} placeholder="Enter Discount %" keyboardType="numeric"/>
+      <Text>You Save : $ {Calculate("s")} </Text>
+      <Text>Final Price : $ {Calculate("f")}</Text>
       <StatusBar style="auto" />
     </View>
   );
